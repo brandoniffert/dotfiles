ecf848d3207d31aa7794dd42ed51ee1d8122eb7c
Brandon Iffert <brandoniffert@gmail.com>
2012-07-17 22:59:50 -0400

    clean up PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/share/python:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.rbenv/shims:/usr/local/share/python:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
