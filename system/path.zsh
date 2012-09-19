0d73add37fbee7060bb0547c814bd26a538aeffe
Brandon Iffert <brandoniffert@gmail.com>
2012-09-18 21:23:38 -0400

    add npm to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.rbenv/shims:/usr/local/share/python:$ZSH/bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.rbenv/shims:/usr/local/share/npm/bin:/usr/local/share/python:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
