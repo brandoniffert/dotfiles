7f495ad57116f9572629eca642f9c88000b6bd1a
Brandon Iffert <brandoniffert@gmail.com>
2012-12-06 15:04:27 -0500

    update zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,13 +1,9 @@
 alias zr!='. ~/.zshrc'
 
-alias c='clear'
-
-# OSX specific directories
+# personal directories
 alias desk='cd $HOME/Desktop'
 alias proj='cd $HOME/Projects'
 alias sites='cd $HOME/Sites'
 alias dot='cd $HOME/.dotfiles'
 
 alias t='tmux -u'
-
-alias create-ss-module='touch _config.php && mkdir code && mkdir -p templates/Includes && mkdir -p templates/Layout && mkdir stylesheets && mkdir -p javascripts/vendor'
