61dcac9947324b9390d9c332d1d0ea914cea0968
Brandon Iffert <brandoniffert@gmail.com>
2013-02-07 00:21:14 -0500

    remove useless aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,11 +1,3 @@
 alias zr!='. ~/.zshrc'
-
-# personal directories
-alias desk='cd $HOME/Desktop'
-alias proj='cd $HOME/Projects'
-alias sites='cd $HOME/Sites'
-alias dot='cd $HOME/.dotfiles'
-
 alias t='tmux -u'
-
 alias g='git'
