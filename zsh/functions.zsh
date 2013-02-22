a07c11819f00e85080ef2187ee51f5cd6d45ae02
Brandon Iffert <brandoniffert@gmail.com>
2013-02-22 01:29:26 -0500

    use a port other than 8000 for server

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,6 +1,6 @@
 # serve a directory on localhost and open in browser
 server() {
-	local port="${1:-8000}"
+	local port="${1:-9876}"
 	open "http://localhost:${port}/"
 	python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
 }
