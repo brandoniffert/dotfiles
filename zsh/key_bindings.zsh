6bd8f25749d2e5dfb6ddfb81a1830dc39436c81e
Brandon Iffert <brandoniffert@gmail.com>
2013-03-11 10:44:03 -0400

    use vi-mode

--- zsh/key_bindings.zsh
+++ zsh/key_bindings.zsh
@@ -1,5 +1,5 @@
 # taken from https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/key-bindings.zsh
-bindkey -e
+bindkey -v
 bindkey '\ew' kill-region
 bindkey -s '\el' "ls\n"
 bindkey '^r' history-incremental-search-backward
@@ -28,4 +28,3 @@ bindkey '^?' backward-delete-char
 bindkey "^[[3~" delete-char
 bindkey "^[3;5~" delete-char
 bindkey "\e[3~" delete-char
-
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -61,4 +61,30 @@ suspended_jobs() {
   fi
 }
 
-export PROMPT=$'$(suspended_jobs)$(current_user) in $(directory_name)$(git_dirty)$(need_push)\n➟ '
+# Ensures that $terminfo values are valid and updates editor information when
+# the keymap changes.
+function zle-keymap-select zle-line-init zle-line-finish {
+  # The terminal must be in application mode when ZLE is active for $terminfo
+  # values to be valid.
+  if (( ${+terminfo[smkx]} )); then
+    printf '%s' ${terminfo[smkx]}
+  fi
+  if (( ${+terminfo[rmkx]} )); then
+    printf '%s' ${terminfo[rmkx]}
+  fi
+
+  zle reset-prompt
+  zle -R
+}
+
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
+
+function vi_mode_prompt_info() {
+  COMMAND_MODE="%{$fg_bold[red]%}➟%{$reset_color%}"
+  INSERT_MODE="➟"
+  echo "${${KEYMAP/vicmd/$COMMAND_MODE}/(main|viins)/$INSERT_MODE}"
+}
+
+export PROMPT=$'$(suspended_jobs)$(current_user) in $(directory_name)$(git_dirty)$(need_push)\n$(vi_mode_prompt_info) '
