d7c4b0fb9994fbf929c2c46d8fbf15e219f4df0e
Brandon Iffert <brandoniffert@gmail.com>
2013-05-13 11:58:58 -0400

    update prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,57 +1,45 @@
-autoload colors && colors
+PROMPT_CARET='❯'
+PROMPT_JOBS='⚙'
+PROMPT_PUSH='●'
 
-git=`which git`
-
-git_dirty() {
-  st=$($git status 2>/dev/null | tail -n 1)
+prompt_git() {
+  st=$(git status 2>/dev/null | tail -n 1)
   if [[ $st == "" ]]
   then
-    echo ""
+    echo -n ""
   else
     if [[ $st == "nothing to commit, working directory clean" ]]
     then
-      echo " (%{$fg[green]%}$(git_prompt_info)%{$reset_color%})"
+      echo -n " (%F{green}%}$(git_prompt_info)%f)"
     else
-      echo " (%{$fg[red]%}$(git_prompt_info)%{$reset_color%})"
+      echo -n " (%F{red}%}$(git_prompt_info)%f)"
     fi
   fi
 }
 
-git_prompt_info () {
-  ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-  echo "${ref#refs/heads/}"
+git_prompt_info() {
+  ref=$(git symbolic-ref HEAD 2>/dev/null) || return
+  echo -n "${ref#refs/heads/}"
 }
 
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
+unpushed() {
+  git cherry -v @{upstream} 2>/dev/null
 }
 
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " %{$fg[red]%}●%{$reset_color%} "
-  fi
+prompt_need_push() {
+  [[ $(unpushed) != "" ]] && echo -n " %F{red}%}$PROMPT_PUSH%f "
 }
 
-directory_name() {
-  echo "%{$fg[blue]%}%~%F%{$reset_color%}"
+prompt_dir() {
+  echo -n "%F{blue}%}%~%f"
 }
 
-current_user() {
-  if [[ $EUID -ne 0 ]]; then
-    echo "%{$fg_bold[green]%}%n%{$reset_color%}"
-  else
-    echo "%{$fg[red]%}root%{$reset_color%}"
-  fi
+prompt_user() {
+  echo -n "%B%F{green}%}%n%f%b"
 }
 
-suspended_jobs() {
-  suspended=$(jobs)
-  if [[ $suspended != "" ]]; then
-    echo "%{$fg_bold[red]%}⚙%{$reset_color%} "
-  fi
+prompt_jobs() {
+  [[ $(jobs -l | wc -l) -gt 0 ]] && echo -n "%B%F{red}%}$PROMPT_JOBS%f%b "
 }
 
-export PROMPT=$'$(suspended_jobs)$(current_user) in $(directory_name)$(git_dirty)$(need_push)\n❯ '
+PROMPT=$'$(prompt_jobs)$(prompt_user):$(prompt_dir)$(prompt_git)$(prompt_need_push)\n$PROMPT_CARET '
