55b220fed205adc5fdaeddb77168f3553e9b3eb7
Brandon Iffert <brandoniffert@gmail.com>
2013-05-19 15:43:14 -0400

    update PATH

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,5 +3,5 @@ export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=0;41:sg=0;4
 export CLICOLOR=true
 export EDITOR='vim'
 
-export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.rbenv/shims:/usr/local/share/npm/bin:/usr/local/share/python:$DOTFILES/bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:/usr/local/share/python:$DOTFILES/bin:$HOME/.scripts:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
