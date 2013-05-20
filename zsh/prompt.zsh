eb2937b1df1e11a1e4331715fb9a8068915f81fc
Brandon Iffert <brandoniffert@gmail.com>
2013-05-20 10:31:21 -0400

    update prompt theme

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,45 +1,97 @@
-PROMPT_CARET='❯'
-PROMPT_JOBS='⚙'
-PROMPT_PUSH='‼'
+# Based off of OMZ agnoster theme
+# https://github.com/robbyrussell/oh-my-zsh/blob/master/themes/agnoster.zsh-theme
 
-prompt_git() {
-  st=$(git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
-  then
-    echo -n ""
+CURRENT_BG='NONE'
+SEGMENT_SEPARATOR=''
+
+### Segment drawing
+# A few utility functions to make it easy and re-usable to draw segmented prompts
+
+# Begin a segment
+# Takes two arguments, background and foreground. Both can be omitted,
+# rendering default background/foreground.
+prompt_segment() {
+  local bg fg
+  [[ -n $1 ]] && bg="%K{$1}" || bg="%k"
+  [[ -n $2 ]] && fg="%F{$2}" || fg="%f"
+  if [[ $CURRENT_BG != 'NONE' && $1 != $CURRENT_BG ]]; then
+    echo -n " %{$bg%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR%{$fg%} "
   else
-    if [[ $st == "nothing to commit, working directory clean" ]]
-    then
-      echo -n " (%F{green}%}$(git_prompt_info)%f)"
-    else
-      echo -n " (%F{red}%}$(git_prompt_info)%f)"
-    fi
+    echo -n "%{$bg%}%{$fg%} "
+  fi
+  CURRENT_BG=$1
+  [[ -n $3 ]] && echo -n $3
+}
+
+# End the prompt, closing any open segments
+prompt_end() {
+  if [[ -n $CURRENT_BG ]]; then
+    echo -n " %{%k%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR"
+  else
+    echo -n "%{%k%}"
+  fi
+  echo -n "%{%f%}"
+  CURRENT_BG=''
+}
+
+### Prompt components
+# Each component will draw itself, and hide itself if no information needs to be shown
+
+# Context: user@hostname (who am I and where am I)
+prompt_context() {
+  local user=`whoami`
+
+  if [[ "$user" != "$DEFAULT_USER" || -n "$SSH_CLIENT" ]]; then
+    prompt_segment black default "%(!.%{%F{yellow}%}.)$user@%m"
   fi
 }
 
-git_prompt_info() {
-  ref=$(git symbolic-ref HEAD 2>/dev/null) || return
-  echo -n "${ref#refs/heads/}"
-}
+prompt_git() {
+  local ref st dirty push
+  if $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
+    st=$(git status 2>/dev/null | tail -n 1)
+    ref=$(git symbolic-ref HEAD 2> /dev/null) || ref="➦ $(git show-ref --head -s --abbrev |head -n1 2> /dev/null)"
 
-unpushed() {
-  git cherry -v @{upstream} 2>/dev/null
-}
+    if [[ $st == "nothing to commit, working directory clean" ]]
+    then
+      prompt_segment green black
+    else
+      dirty=' ±'
+      prompt_segment yellow black
+    fi
 
-prompt_need_push() {
-  [[ $(unpushed) != "" ]] && echo -n " %F{red}%}$PROMPT_PUSH%f "
+    [[ $(git cherry -v @{upstream} 2>/dev/null) != "" ]] && push=' !!'
+
+    echo -n "${ref/refs\/heads\// }$dirty$push"
+  fi
 }
 
 prompt_dir() {
-  echo -n "%F{blue}%}%~%f"
+  prompt_segment blue black '%~'
 }
 
-prompt_user() {
-  echo -n "%B%F{green}%}%n%f%b"
+# Status:
+# - was there an error
+# - am I root
+# - are there background jobs?
+prompt_status() {
+  local symbols
+  symbols=()
+  [[ $RETVAL -ne 0 ]] && symbols+="%{%F{red}%}✘"
+  [[ $UID -eq 0 ]] && symbols+="%{%F{yellow}%}⚡"
+  [[ $(jobs -l | wc -l) -gt 0 ]] && symbols+="%{%F{cyan}%}⚙"
+
+  [[ -n "$symbols" ]] && prompt_segment black default "$symbols"
 }
 
-prompt_jobs() {
-  [[ $(jobs -l | wc -l) -gt 0 ]] && echo -n "%B%F{red}%}$PROMPT_JOBS%f%b "
+## Main prompt
+build_prompt() {
+  RETVAL=$?
+  prompt_status
+  prompt_context
+  prompt_dir
+  prompt_git
+  prompt_end
 }
 
-PROMPT=$'$(prompt_jobs)$(prompt_user):$(prompt_dir)$(prompt_git)$(prompt_need_push)\n$PROMPT_CARET '
+PROMPT='%{%f%b%k%}$(build_prompt) '
