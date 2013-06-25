da786d70d38c1222081e09f7b88080b5c4ade398
Brandon Iffert <brandoniffert@gmail.com>
2013-06-24 23:00:08 -0400

    move profile background color into env variable

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -5,3 +5,7 @@ export EDITOR='vim'
 
 export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:/usr/local/share/python:$DOTFILES/bin:$HOME/.scripts:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# let programs know whether the term bg is light or dark
+[[ $ITERM_PROFILE == *"light"* ]] && PROFILE_BG='light' || PROFILE_BG='dark'
+export PROFILE_BG
