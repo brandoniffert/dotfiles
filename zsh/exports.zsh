b33e75acb7bc0c6b1bcef4aebf4a41de096450aa
Brandon Iffert <brandoniffert@gmail.com>
2013-09-20 22:46:46 -0400

    remove python from PATH. homebrew no longer needs it

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,7 +3,7 @@ export LS_COLORS="di=94:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=0;41:sg=0;4
 export CLICOLOR=true
 export EDITOR='vim'
 
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:/usr/local/share/python:$DOTFILES/bin:$HOME/.scripts:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$DOTFILES/bin:$HOME/.scripts:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 # let programs know whether the term bg is light or dark
