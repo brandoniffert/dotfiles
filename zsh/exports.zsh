b3e95e46fec10071a4b7db42c2de4bd5e502dcc3
Brandon Iffert <brandoniffert@gmail.com>
2013-12-19 12:03:38 -0500

    move PATH into separate file

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,9 +2,6 @@ export LSCOLORS="ExGxBxDxCxEgEdxbxgxcxd"
 export CLICOLOR=true
 export EDITOR='vim'
 
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$DOTFILES/bin:$HOME/.scripts:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
 # let programs know whether the term bg is light or dark
 [[ $ITERM_PROFILE == *"light"* ]] && PROFILE_BG='light' || PROFILE_BG='dark'
 export PROFILE_BG
---
+++ zsh/path.zsh
@@ -0,0 +1,3 @@
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$DOTFILES/bin:$HOME/.scripts:$PATH"
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
