67be07bee7ac4ad030c212de33f9afa904856858
Brandon Iffert <brandoniffert@gmail.com>
2014-02-01 13:27:37 -0500

    use base16 - 256 variation

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -5,3 +5,8 @@ export EDITOR='vim'
 # let programs know whether the term bg is light or dark
 [[ $ITERM_PROFILE == *"light"* ]] && PROFILE_BG='light' || PROFILE_BG='dark'
 export PROFILE_BG
+
+[[ $PROFILE_BG == "light" ]] && BASE16_SCHEME='solarized' || BASE16_SCHEME='default'
+BASE16_SHELL="$HOME/.config/base16-shell/base16-$BASE16_SCHEME.dark.sh"
+[[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
+
