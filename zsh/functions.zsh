2c436b42dad63223e03ccc610f3ef5cece6e01ec
Brandon Iffert <brandoniffert@gmail.com>
2014-02-03 23:06:09 -0500

    clean up functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,7 +2,7 @@
 function server() {
 	local port="${1:-9876}"
 	open "http://localhost:${port}/"
-	python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
+  python -m SimpleHTTPServer $port
 }
 
 # serve a directory on localhost and open in browser (php)
@@ -12,9 +12,9 @@ function pserver() {
   php -S localhost:$port
 }
 
-# Extract archives - use: extract <file>
+# Extract archives - use: xtract <file>
 # Credits to http://dotfiles.org/~pseup/.bashrc
-function extract() {
+function xtract() {
 	if [ -f $1 ] ; then
 		case $1 in
 			*.tar.bz2) tar xjf $1 ;;
@@ -28,7 +28,7 @@ function extract() {
 			*.zip) unzip $1 ;;
 			*.Z) uncompress $1 ;;
 			*.7z) 7z x $1 ;;
-			*) echo "'$1' cannot be extracted via extract()" ;;
+			*) echo "'$1' cannot be extracted via xtract()" ;;
 		esac
 	else
 		echo "'$1' is not a valid file"
