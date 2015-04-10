6385eaaca5dc90d694631ff4574c63eda06556e3
Brandon Iffert <brandoniffert@gmail.com>
2015-04-10 17:12:15 -0400

    tweak man colors

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -69,7 +69,7 @@ function man() {
     LESS_TERMCAP_md=$(printf "\e[1;31m") \
     LESS_TERMCAP_me=$(printf "\e[0m") \
     LESS_TERMCAP_se=$(printf "\e[0m") \
-    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_so=$(printf "\e[1;37;41m") \
     LESS_TERMCAP_ue=$(printf "\e[0m") \
     LESS_TERMCAP_us=$(printf "\e[1;32m") \
     man "$@"
