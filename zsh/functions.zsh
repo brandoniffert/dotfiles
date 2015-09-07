939354f4de61e12dd15827f37df262232aeccf73
Brandon Iffert <brandoniffert@gmail.com>
2015-09-07 11:46:24 -0400

    don't use bold colors in man

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -65,13 +65,13 @@ function fs() {
 # Colorize man pages
 function man() {
   env \
-    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_mb=$(printf "\e[0;31m") \
+    LESS_TERMCAP_md=$(printf "\e[0;31m") \
     LESS_TERMCAP_me=$(printf "\e[0m") \
     LESS_TERMCAP_se=$(printf "\e[0m") \
-    LESS_TERMCAP_so=$(printf "\e[1;37;41m") \
+    LESS_TERMCAP_so=$(printf "\e[0;37;41m") \
     LESS_TERMCAP_ue=$(printf "\e[0m") \
-    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    LESS_TERMCAP_us=$(printf "\e[0;32m") \
     man "$@"
 }
 
