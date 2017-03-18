c2a32254fa52c0ffb38cbd4a13f47e75e03fd3e3
Brandon Iffert <brandoniffert@gmail.com>
2017-03-18 14:54:27 -0400

    Add git_super_status

---
+++ zsh/git-super-status.zsh
@@ -0,0 +1,89 @@
+# Modified version of https://github.com/olivierverdier/zsh-git-prompt
+
+autoload -U add-zsh-hook
+
+add-zsh-hook chpwd chpwd_update_git_vars
+add-zsh-hook preexec preexec_update_git_vars
+add-zsh-hook precmd precmd_update_git_vars
+
+## Function definitions
+function preexec_update_git_vars() {
+  case "$2" in
+    git*|hub*|gh*|stg*)
+      __EXECUTED_GIT_COMMAND=1
+      ;;
+  esac
+}
+
+function precmd_update_git_vars() {
+  if [ -n "$__EXECUTED_GIT_COMMAND" ] || [ ! -n "$ZSH_THEME_GIT_PROMPT_CACHE" ]; then
+    update_current_git_vars
+    unset __EXECUTED_GIT_COMMAND
+  fi
+}
+
+function chpwd_update_git_vars() {
+  update_current_git_vars
+}
+
+function update_current_git_vars() {
+  unset __CURRENT_GIT_STATUS
+
+  _GIT_STATUS=`git status --porcelain --branch &> /dev/null | gitstatus`
+  __CURRENT_GIT_STATUS=("${(@s: :)_GIT_STATUS}")
+
+  GIT_BRANCH=$__CURRENT_GIT_STATUS[1]
+  GIT_AHEAD=$__CURRENT_GIT_STATUS[2]
+  GIT_BEHIND=$__CURRENT_GIT_STATUS[3]
+  GIT_STAGED=$__CURRENT_GIT_STATUS[4]
+  GIT_CONFLICTS=$__CURRENT_GIT_STATUS[5]
+  GIT_CHANGED=$__CURRENT_GIT_STATUS[6]
+  GIT_UNTRACKED=$__CURRENT_GIT_STATUS[7]
+}
+
+function git_super_status() {
+  precmd_update_git_vars
+  if [ -n "$__CURRENT_GIT_STATUS" ]; then
+    STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"
+
+    if [ "$GIT_BEHIND" -ne "0" ]; then
+      STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_BEHIND$GIT_BEHIND%{${reset_color}%}"
+    fi
+    if [ "$GIT_AHEAD" -ne "0" ]; then
+      STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_AHEAD$GIT_AHEAD%{${reset_color}%}"
+    fi
+
+    if [ "$GIT_CHANGED" -ne "0" ] || [ "$GIT_CONFLICTS" -ne "0" ] || [ "$GIT_STAGED" -ne "0" ] || [ "$GIT_UNTRACKED" -ne "0" ]; then
+      STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_SEPARATOR"
+
+      if [ "$GIT_STAGED" -ne "0" ]; then
+        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_STAGED$GIT_STAGED%{${reset_color}%}"
+      fi
+      if [ "$GIT_CONFLICTS" -ne "0" ]; then
+        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CONFLICTS$GIT_CONFLICTS%{${reset_color}%}"
+      fi
+      if [ "$GIT_CHANGED" -ne "0" ]; then
+        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_CHANGED$GIT_CHANGED%{${reset_color}%}"
+      fi
+      if [ "$GIT_UNTRACKED" -ne "0" ]; then
+        STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_UNTRACKED%{${reset_color}%}"
+      fi
+    fi
+
+    STATUS="$STATUS%{${reset_color}%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+
+    echo -n " $STATUS"
+  fi
+}
+
+# Default values for the appearance of the prompt. Configure at will.
+ZSH_THEME_GIT_PROMPT_PREFIX="%F{7}("
+ZSH_THEME_GIT_PROMPT_SUFFIX="%F{7})"
+ZSH_THEME_GIT_PROMPT_SEPARATOR="%F{7}|"
+ZSH_THEME_GIT_PROMPT_BRANCH="%F{7}"
+ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[red]%}%{●%G%}"
+ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}%{!%G%}"
+ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[blue]%}%{+%G%}"
+ZSH_THEME_GIT_PROMPT_BEHIND="%{↓%G%}"
+ZSH_THEME_GIT_PROMPT_AHEAD="%{↑%G%}"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[red]%}?%G%}"
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,72 +1,36 @@
-autoload -Uz vcs_info
+source "$DOTFILES/zsh/git-super-status.zsh"
 
-# Setup git
-zstyle ':vcs_info:*' enable git
-
-# Git info styles
-zstyle ':vcs_info:git*' unstagedstr '%F{red}●%f'
-zstyle ':vcs_info:git*' stagedstr '%F{green}●%f'
-zstyle ':vcs_info:git*' formats '%F{7}(%b%u%c%m%F{7})%f'
-zstyle ':vcs_info:git*' actionformats '%F{7}(%b%u%c%m:%F{green}%a%F{7})%f'
-zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-ahead
-zstyle ':vcs_info:git*' check-for-changes true
-
-+vi-git-untracked(){
-  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
-      git status --porcelain | fgrep '??' &> /dev/null ; then
-    hook_com[staged]+='%F{red}?%f'
-  fi
+function prompt_divider() {
+  echo "\x1b[38;2;49;53;69m$(repeat $COLUMNS printf "%s" "—")\x1b[0m"
 }
 
-+vi-git-ahead() {
-  local ahead
-  local -a gitstatus
-
-  ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2>/dev/null | wc -l | xargs)
-  [ $ahead -ne 0 ] && gitstatus+="%F{red}!%f"
-  hook_com[misc]+=${gitstatus}
+function prompt_virtualenv() {
+  [[ ! -z "$VIRTUAL_ENV" ]] && echo -n "(`basename \"$VIRTUAL_ENV\"`) "
 }
 
-precmd() {
-  vcs_info
-}
-
-prompt_jobs() {
+function prompt_jobs() {
   echo -n "%F{7}%(1j.%j .)%f"
 }
 
-prompt_dir() {
+function prompt_dir() {
   echo -n '%F{cyan}%c%f'
 }
 
-prompt_git() {
-  local git_status="${vcs_info_msg_0_}"
-  [[ $git_status != '' ]] && echo -n " $git_status"
+function prompt_marker() {
+  echo "%F{yellow}%(!.#.:) %f"
 }
 
-prompt_vagrant_status() {
+function prompt_vagrant_status() {
   local running="$(cat ~/.vagrant-global-status 2> /dev/null)"
   [ -n "$running" ] && echo -n "%F{7}[$running]%f"
 }
 
-prompt_marker() {
-  echo "%F{red}%(!.#.:) %f"
-}
-
-prompt_divider() {
-  echo "\x1b[38;2;39;43;59m$(repeat $COLUMNS printf "%s" "—")\x1b[0m"
-}
-
-prompt_virtualenv() {
-  [[ ! -z "$VIRTUAL_ENV" ]] && echo -n "(`basename \"$VIRTUAL_ENV\"`) "
-}
-
-build_prompt() {
+function build_prompt() {
   prompt_divider
   prompt_virtualenv
   prompt_jobs
   prompt_dir
-  prompt_git
+  git_super_status
   prompt_marker
 }
 
