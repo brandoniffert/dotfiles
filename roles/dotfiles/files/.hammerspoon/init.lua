9ba53afe5cf8dab49a73993c38b2d98bf7ca1f19
Brandon Iffert <brandoniffert@gmail.com>
2019-06-25 08:54:11 -0400

    hammerspoon: Setup hyper key bindings, remove fullscreen binding

--- roles/dotfiles/files/.hammerspoon/init.lua
+++ roles/dotfiles/files/.hammerspoon/init.lua
@@ -2,32 +2,25 @@ hs.window.animationDuration = 0
 
 local log = require 'log'
 
+-- Hyper key
 local hyper = {'cmd', 'alt', 'ctrl', 'shift'}
+
+bindHyper = function(fromKey, toKey)
+  sendKey = function()
+    hs.eventtap.event.newKeyEvent({}, toKey, true):post()
+  end
+
+  hs.hotkey.bind(hyper, fromKey, sendKey, nil, sendKey)
+end
+
+bindHyper('f', 'up')
+bindHyper('s', 'down')
+bindHyper('r', 'left')
+bindHyper('t', 'right')
 
 -- Put displays to sleep
 hs.hotkey.bind(hyper, '`', function()
   os.execute('pmset displaysleepnow')
-end)
-
--- Fullscreen current window (non-native fullscreen)
-hs.hotkey.bind(hyper, 'return', function()
-  local win = hs.window.focusedWindow()
-  local frame = win:frame()
-  local max = win:screen():frame()
-  local uberscicht = hs.application.find('Übersicht')
-
-  frame.x = max.x + 10
-  frame.y = max.y + 10
-  frame.w = max.w - 20
-  frame.h = max.h - 20
-
-  -- If uberscicht is running, need to adjust for the bar on the bottom of the screen
-  -- The bar is 30px tall, so subtract that from the frame height
-  if uberscicht ~= nil then
-    frame.h = frame.h - 30 - 10
-  end
-
-  win:setFrame(frame)
 end)
 
 -- Move window between screens
