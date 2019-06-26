a08323e953ffede7f50a90d8bb30dde01b431393
Brandon Iffert <brandoniffert@gmail.com>
2019-06-26 15:44:56 -0400

    hammerspoon: Keep the current screens up to date

--- roles/dotfiles/files/.hammerspoon/init.lua
+++ roles/dotfiles/files/.hammerspoon/init.lua
@@ -35,8 +35,15 @@ bindHyperFn('`', function()
   os.execute('pmset displaysleepnow')
 end)
 
+-- Get list of screens and refresh that list whenever screens are (un)plugged
+local screens = hs.screen.allScreens()
+local screenwatcher = hs.screen.watcher.new(function()
+  screens = hs.screen.allScreens()
+end)
+screenwatcher:start()
+
 -- Move window between screens
-local cycleScreens = hs.fnutils.cycle(hs.screen.allScreens())
+local cycleScreens = hs.fnutils.cycle(screens)
 
 bindHyperFn('tab', function()
   hs.window.focusedWindow():moveToScreen(cycleScreens())
