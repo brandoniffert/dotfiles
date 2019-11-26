bebb9736e8e9fddd8ce1b79d575e5b4801fba79d
Brandon Iffert <brandoniffert@gmail.com>
2019-11-26 08:09:13 -0500

    zsh: Update prompt markers

--- roles/dotfiles/files/.zsh/prompt.zsh
+++ roles/dotfiles/files/.zsh/prompt.zsh
@@ -4,8 +4,8 @@ autoload -U colors && colors
 
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' stagedstr "%F{green}●%f"
-zstyle ':vcs_info:*' unstagedstr "%F{red}●%f"
+zstyle ':vcs_info:*' stagedstr "%F{green}▪%f"
+zstyle ':vcs_info:*' unstagedstr "%F{red}▪%f"
 zstyle ':vcs_info:*' use-simple true
 zstyle ':vcs_info:git+set-message:*' hooks git-untracked git-aheadbehind git-stash
 zstyle ':vcs_info:git*:*' formats '%F{black}[%f%b%m%c%u%F{black}]%f '
@@ -14,7 +14,7 @@ zstyle ':vcs_info:git*:*' actionformats '%b|%a%m%c%u '
 function +vi-git-untracked() {
   emulate -L zsh
   if [[ -n $(git ls-files --exclude-standard --others 2> /dev/null) ]]; then
-    hook_com[unstaged]+="%F{yellow}●%f"
+    hook_com[unstaged]+="%F{yellow}▪%f"
   fi
 }
 
@@ -61,12 +61,12 @@ function () {
   local MARKER="%(?.%F{green}.%F{red})⁕%f "
   local SSHTTY="%F{green}${SSH_TTY:+%n@%m}%f%B${SSH_TTY:+:}%b"
   local DIR="%F{blue}%1~%f "
-  local JOBS="%F{yellow}%(1j.* .)%f"
+  local JOBS="%F{yellow}%(1j.⁕ .)%f"
 
   if [[ $EUID -eq 0 ]]; then
     local SUFFIX="%F{red}$(printf '#%.0s' {1..$LVL})%f"
   else
-    local SUFFIX="%F{white}$(printf '❯%.0s' {1..$LVL})%f"
+    local SUFFIX="%F{white}$(printf '$%.0s' {1..$LVL})%f"
   fi
 
   export PS1="${MARKER}${SSHTTY}\$(venv_info)${DIR}\${vcs_info_msg_0_%%}${JOBS}${SUFFIX} "
