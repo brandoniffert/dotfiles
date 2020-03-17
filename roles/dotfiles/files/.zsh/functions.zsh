12e3bf64be10ec226f2a9f665d5b2047cf322dd9
Brandon Iffert <brandoniffert@gmail.com>
2020-03-17 11:54:00 -0400

    zsh: Tweak man page colors

--- roles/dotfiles/files/.zsh/functions.zsh
+++ roles/dotfiles/files/.zsh/functions.zsh
@@ -36,12 +36,13 @@ function fs() {
 
 # Colorize man pages
 function man() {
+  LESS_TERMCAP_mb=$'\E[01;31m' \
   LESS_TERMCAP_md=$'\e[01;34m' \
   LESS_TERMCAP_me=$'\e[0m' \
   LESS_TERMCAP_se=$'\e[0m' \
-  LESS_TERMCAP_so=$'\e[01;44;30m' \
+  LESS_TERMCAP_so=$'\e[01;43;30m' \
   LESS_TERMCAP_ue=$'\e[0m' \
-  LESS_TERMCAP_us=$'\e[01;32m' \
+  LESS_TERMCAP_us=$'\e[04;32m' \
   command man "$@"
 }
 
