7a64f570ac37c9de2decef9b9fb2efeb2b9fa45a
Brandon Iffert <brandoniffert@gmail.com>
2020-03-27 07:56:07 -0400

    zsh: Clear screen when using ts function

--- roles/dotfiles/files/.zsh/functions.zsh
+++ roles/dotfiles/files/.zsh/functions.zsh
@@ -142,6 +142,7 @@ function ts() {
   local session
   session=$(tmux list-sessions -F "#{session_name}" | \
     fzf --query="$1" --select-1 --exit-0) &&
+  clear &&
   tmux switch-client -t "$session"
 }
 
