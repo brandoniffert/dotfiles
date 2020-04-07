028cf6f4b8e07bfdd7e78ae64fa74b4e321fd631
Brandon Iffert <brandoniffert@gmail.com>
2020-04-07 12:03:05 -0400

    feat(hammerspoon): use meh key instead of hyper

--- dotfiles/hammerspoon/init.lua
+++ dotfiles/hammerspoon/init.lua
@@ -3,42 +3,42 @@ local log = require('log')
 
 hs.window.animationDuration = 0
 
--- Hyper key
-local hyper = {'cmd', 'alt', 'ctrl', 'shift'}
+-- Meh key
+local meh = {'alt', 'ctrl', 'shift'}
 
 fastKeyStroke = function(modifiers, character)
   hs.eventtap.event.newKeyEvent(modifiers, string.lower(character), true):post()
   hs.eventtap.event.newKeyEvent(modifiers, string.lower(character), false):post()
 end
 
-bindHyper = function(fromKey, toKey)
-  hs.hotkey.bind(hyper, fromKey,
+bindMeh = function(fromKey, toKey)
+  hs.hotkey.bind(meh, fromKey,
     function() fastKeyStroke({}, toKey) end,
     nil,
     function() fastKeyStroke({}, toKey) end
   )
 end
 
-bindHyperFn = function(fromKey, func)
-  hs.hotkey.bind(hyper, fromKey, func)
+bindMehFn = function(fromKey, func)
+  hs.hotkey.bind(meh, fromKey, func)
 end
 
 -- Put displays to sleep
-bindHyperFn('`', function()
+bindMehFn('`', function()
   os.execute('pmset displaysleepnow')
 end)
 
 -- Window management
-bindHyperFn('r', WinMan.moveWindowLeft)
-bindHyperFn('t', WinMan.moveWindowRight)
-bindHyperFn('w', WinMan.moveWindowUpperLeft)
-bindHyperFn('x', WinMan.moveWindowBottomLeft)
-bindHyperFn('p', WinMan.moveWindowUpperRight)
-bindHyperFn('v', WinMan.moveWindowBottomRight)
-bindHyperFn('tab', WinMan.moveWindowToNextMonitor)
-bindHyperFn('return', WinMan.maximizeWindow)
-bindHyperFn('delete', WinMan.undoPop)
-bindHyperFn('space', WinMan.centerWindow)
+bindMehFn('r', WinMan.moveWindowLeft)
+bindMehFn('t', WinMan.moveWindowRight)
+bindMehFn('w', WinMan.moveWindowUpperLeft)
+bindMehFn('x', WinMan.moveWindowBottomLeft)
+bindMehFn('p', WinMan.moveWindowUpperRight)
+bindMehFn('v', WinMan.moveWindowBottomRight)
+bindMehFn('tab', WinMan.moveWindowToNextMonitor)
+bindMehFn('return', WinMan.maximizeWindow)
+bindMehFn('delete', WinMan.undoPop)
+bindMehFn('space', WinMan.centerWindow)
 
 -- Application watcher
 function appLaunched(appName, eventType, app)
