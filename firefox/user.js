5a3f24fd68522d680446833042140ae6b7c8e7e2
Brandon Iffert <brandoniffert@gmail.com>
2020-04-15 06:13:17 -0400

    feat(firefox): add user.js preferences

---
+++ firefox/user.js
@@ -0,0 +1,16 @@
+user_pref("gfx.webrender.all", true);
+user_pref("nglayout.initialpaint.delay", 0);
+user_pref("content.notify.ontimer", false);
+user_pref("content.interrupt.parsing", false);
+user_pref("network.http.max-persistent-connections-per-server", 10);
+user_pref("network.http.max-connections", 2000);
+user_pref("network.manage-offline-status", false);
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("security.dialog_enable_delay", 0);
+
+user_pref("full-screen-api.warning.delay", 0);
+user_pref("full-screen-api.warning.timeout", 0);
+user_pref("browser.aboutConfig.showWarning", false);
+
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
