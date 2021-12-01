f8844546fbf4496980e38057fc3bd107495d49de
Brandon Iffert <brandoniffert@gmail.com>
2021-12-01 06:30:27 -0500

    chore(nvim): update plugin repo

--- nvim/lua/bti/plugins.lua
+++ nvim/lua/bti/plugins.lua
@@ -40,7 +40,7 @@ require('packer').startup({function()
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
-      { 'andersevenrud/compe-tmux', branch = 'cmp' },
+      { 'andersevenrud/cmp-tmux' },
       'L3MON4D3/LuaSnip'
     },
     config = [[require('bti.config.nvim-cmp')]]
