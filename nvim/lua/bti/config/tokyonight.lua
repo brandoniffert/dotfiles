69bb7ac27a05aabbc1b4475adf1863cd40e312cf
Brandon Iffert <brandoniffert@gmail.com>
2021-12-02 08:21:13 -0500

    refactor: adjust colors to be closer to default tokyonight

--- nvim/lua/bti/config/tokyonight.lua
+++ nvim/lua/bti/config/tokyonight.lua
@@ -1,18 +1,11 @@
 vim.g.tokyonight_style = 'night'
 vim.g.tokyonight_lualine_bold = true
-vim.g.tokyonight_dark_sidebar = false
 vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_dark_float = false
 vim.g.tokyonight_colors = {
-  bg = '#171926',
-  bg_float = '#151723',
-  border = 'bg_highlight',
-  bg_visual = 'bg_highlight',
+  bg_visual = 'bg_highlight'
 }
 
-vim.cmd [[colorscheme tokyonight]]
+require('tokyonight').colorscheme()
 
-vim.cmd [[hi Tabline guifg=#6c759d guibg=#171926]]
-vim.cmd [[hi TablineFill guibg=#171926]]
-vim.cmd [[hi TablineSel guifg=#c0caf5 guibg=#171926]]
-vim.cmd [[hi InactiveWindow guifg=#565f89 guibg=#131520]]
+vim.cmd [[hi InactiveWindow guifg=#565f89 guibg=#171821]]
