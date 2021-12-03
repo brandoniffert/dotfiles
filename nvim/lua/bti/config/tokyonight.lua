ee27e9f05155f54523208988e7fafd88aea16bf0
Brandon Iffert <brandoniffert@gmail.com>
2021-12-03 07:54:50 -0500

    feat(nvim): use dark sidebars

--- nvim/lua/bti/config/tokyonight.lua
+++ nvim/lua/bti/config/tokyonight.lua
@@ -1,6 +1,6 @@
 vim.g.tokyonight_style = 'night'
 vim.g.tokyonight_lualine_bold = true
-vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_dark_sidebar = true
 vim.g.tokyonight_dark_float = false
 vim.g.tokyonight_colors = {
   bg_visual = 'bg_highlight'
