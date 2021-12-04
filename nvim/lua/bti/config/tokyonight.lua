dd8e5e02ea41d4d55538732e13edbce3ba8fa576
Brandon Iffert <brandoniffert@gmail.com>
2021-12-04 09:29:08 -0500

    feat(nvim): adjust colors

--- nvim/lua/bti/config/tokyonight.lua
+++ nvim/lua/bti/config/tokyonight.lua
@@ -1,7 +1,5 @@
 vim.g.tokyonight_style = 'night'
 vim.g.tokyonight_lualine_bold = true
-vim.g.tokyonight_dark_sidebar = true
-vim.g.tokyonight_dark_float = true
 vim.g.tokyonight_colors = {
   bg_visual = 'bg_highlight'
 }
