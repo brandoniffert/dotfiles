a9d57ee9d6f1a8a6baa12d03420216ca4d79d793
Brandon Iffert <brandoniffert@gmail.com>
2021-12-05 23:24:47 -0500

    feat(nvim): add abbreviations

--- nvim/init.lua
+++ nvim/init.lua
@@ -229,6 +229,10 @@ nnoremap('<LocalLeader>pH',  ':echomsg v:lua.bti.functions.get_highlight_group()
 -- Print the TS highlighting group(s) that apply at the current cursor position
 nnoremap('<LocalLeader>ph',  '<cmd>TSHighlightCapturesUnderCursor<CR>')
 
+-- Abbreviations
+vim.cmd([[iabbrev ;- ->]])
+vim.cmd([[iabbrev ;= =>]])
+
 -------------------------------------------------------------------------------
 -- Autocommands ---------------------------------------------------------------
 -------------------------------------------------------------------------------
