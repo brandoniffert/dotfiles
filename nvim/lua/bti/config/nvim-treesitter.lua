8e8ba0d6e0e33847de6543202be36751793de387
Brandon Iffert <brandoniffert@gmail.com>
2022-04-10 13:35:04 -0400

    chore(nvim): used new ensure_installed setting, but ignore phpdoc for the moment (could not install correctly)

--- nvim/lua/bti/config/nvim-treesitter.lua
+++ nvim/lua/bti/config/nvim-treesitter.lua
@@ -9,7 +9,8 @@ return {
   },
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = "maintained",
+      ensure_installed = "all",
+      ignore_install = { "phpdoc" },
       highlight = {
         enable = true,
       },
