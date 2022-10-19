5012b9c44a847adf87e0f1018c57689249a2c739
Brandon Iffert <brandoniffert@gmail.com>
2022-10-19 08:44:41 -0400

    chore(nvim): use default_capabilities form cmp-nvim-lsp

--- nvim/lua/bti/config/lsp/init.lua
+++ nvim/lua/bti/config/lsp/init.lua
@@ -66,7 +66,7 @@ local servers = {
   },
 }
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local options = {
   on_attach = on_attach,
