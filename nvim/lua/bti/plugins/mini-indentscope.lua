76b2c0800583ad8b0496fef2c622d24ed8f7990f
Brandon Iffert <brandoniffert@gmail.com>
2023-10-20 15:17:48 -0400

    feat(nvim): disable mini.indentscope for certain filetypes and buftypes

--- nvim/lua/bti/plugins/mini-indentscope.lua
+++ nvim/lua/bti/plugins/mini-indentscope.lua
@@ -1,5 +1,37 @@
+local excluded_buftypes = {
+  "nofile",
+  "prompt",
+  "quickfix",
+  "terminal",
+}
+
 return {
   "echasnovski/mini.indentscope",
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      desc = "Disable indentscope for certain filetypes",
+      pattern = {
+        "checkhealth",
+        "help",
+        "lspinfo",
+        "neo-tree",
+        "Trouble",
+        "lazy",
+      },
+      callback = function(event)
+        vim.b[event.buf].miniindentscope_disable = true
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+      desc = "Disable indentscope for certain buftypes",
+      callback = function(event)
+        if vim.tbl_contains(excluded_buftypes, vim.bo[event.buf].buftype) then
+          vim.b[event.buf].miniindentscope_disable = true
+        end
+      end,
+    })
+  end,
   config = function()
     require("mini.indentscope").setup({
       draw = {
