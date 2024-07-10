12d9da6f0464ae657b11b9455e8196053fb67f41
Brandon Iffert <brandoniffert@gmail.com>
2024-07-10 12:12:32 -0400

    feat(nvim): update trouble keymaps for new version

--- nvim/lua/bti/plugins/trouble.lua
+++ nvim/lua/bti/plugins/trouble.lua
@@ -1,12 +1,37 @@
 return {
   "folke/trouble.nvim",
+  cmd = "Trouble",
+  opts = {},
   keys = {
-    { "<Leader>xx", "<cmd>TroubleToggle<CR>", desc = "Toggle" },
-    { "<Leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Workspace diagnostics" },
-    { "<Leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Document diagnostics" },
-    { "<Leader>xr", "<cmd>TroubleToggle lsp_references<CR>", desc = "References" },
-  },
-  opts = {
-    use_diagnostic_signs = true,
+    {
+      "<Leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<Leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<Leader>xs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<Leader>xl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<Leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<Leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
   },
 }
