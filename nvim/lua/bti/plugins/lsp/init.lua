38670a171df013334c5f6ca007cff5d7c352d045
Brandon Iffert <brandoniffert@gmail.com>
2024-07-12 05:45:39 -0400

    chore(nvim): move lazydev to separate file

--- nvim/lua/bti/plugins/lsp/init.lua
+++ nvim/lua/bti/plugins/lsp/init.lua
@@ -6,17 +6,6 @@ return {
     {
       "hrsh7th/cmp-nvim-lsp",
     },
-    {
-      "folke/lazydev.nvim",
-      ft = "lua",
-      opts = {
-        library = {
-          { path = "luvit-meta/library", words = { "vim%.uv" } },
-          { path = "lazy.nvim", words = { "Lazy" } },
-        },
-      },
-    },
-    { "Bilal2453/luvit-meta", lazy = true },
   },
   config = function()
     -- vim.lsp.set_log_level('debug')
