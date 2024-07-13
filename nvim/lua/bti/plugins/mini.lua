49c93a89f1c3814ee9c21e7eab2562e99f3fea6a
Brandon Iffert <brandoniffert@gmail.com>
2024-07-13 02:54:58 -0400

    refactor(nvim): inline excluded buftypes

--- nvim/lua/bti/plugins/mini.lua
+++ nvim/lua/bti/plugins/mini.lua
@@ -2,13 +2,6 @@
 return {
   "echasnovski/mini.nvim",
   init = function()
-    local excluded_buftypes = {
-      "nofile",
-      "prompt",
-      "quickfix",
-      "terminal",
-    }
-
     vim.api.nvim_create_autocmd("FileType", {
       desc = "Disable indentscope for certain filetypes",
       pattern = {
@@ -28,7 +21,7 @@ return {
     vim.api.nvim_create_autocmd("BufWinEnter", {
       desc = "Disable indentscope for certain buftypes",
       callback = function(event)
-        if vim.tbl_contains(excluded_buftypes, vim.bo[event.buf].buftype) then
+        if vim.tbl_contains({ "nofile", "prompt", "quickfix", "terminal" }, vim.bo[event.buf].buftype) then
           vim.b[event.buf].miniindentscope_disable = true
         end
       end,
