496020a3b1044e3aa9e9876f54344eb435ba0416
Brandon Iffert <brandoniffert@gmail.com>
2024-09-22 05:21:08 -0400

    feat(nvim): add includePaths and files settings for intelephense

--- nvim/lua/bti/plugins/lsp/init.lua
+++ nvim/lua/bti/plugins/lsp/init.lua
@@ -72,6 +72,22 @@ return {
             licenceKey = os.getenv("INTELEPHENSE_LICENCE_KEY") or "",
           },
           single_file_support = true,
+          settings = {
+            intelephense = {
+              environment = {
+                includePaths = {
+                  os.getenv("XDG_CONFIG_HOME") .. "/composer/vendor/php-stubs",
+                },
+              },
+              files = {
+                maxSize = 10000000,
+                exclude = {
+                  "**/.git/**",
+                  "**/node_modules/**",
+                },
+              },
+            },
+          },
         },
         jsonls = {
           init_options = {
