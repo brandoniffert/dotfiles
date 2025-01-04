8109c54292824b9a72b7cf49f04586239aa08b3b
Brandon Iffert <brandoniffert@gmail.com>
2025-01-04 04:38:04 -0500

    feat(nvim): update harpoon keymaps

--- nvim/lua/bti/plugins/harpoon.lua
+++ nvim/lua/bti/plugins/harpoon.lua
@@ -2,8 +2,8 @@
 return {
   "ThePrimeagen/harpoon",
   keys = {
-    { "<Leader>a", "<cmd>lua require('harpoon.mark').add_file()<CR>", desc = "Harpoon (Add Mark)" },
-    { "<Leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "Harpoon" },
+    { "<Leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>", desc = "Harpoon (Add Mark)" },
+    { "<Leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "Show Harpoon" },
     { "<Leader>hn", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>", desc = "Harpoon (1)" },
     { "<Leader>he", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>", desc = "Harpoon (2)" },
     { "<Leader>hi", "<cmd>lua require('harpoon.ui').nav_file(3)<CR>", desc = "Harpoon (3)" },
