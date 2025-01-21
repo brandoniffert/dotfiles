d3cdd50c0df48e9e910c70cfce64829c4d40ca23
Brandon Iffert <brandoniffert@gmail.com>
2025-01-21 09:28:29 -0500

    feat(nvim): use latest harpoon

--- nvim/lua/bti/plugins/harpoon.lua
+++ nvim/lua/bti/plugins/harpoon.lua
@@ -1,12 +1,39 @@
 ---@type LazySpec
 return {
   "ThePrimeagen/harpoon",
-  keys = {
-    { "<Leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>", desc = "Harpoon (Add Mark)" },
-    { "<Leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "Show Harpoon" },
-    { "<Leader>hn", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>", desc = "Harpoon (1)" },
-    { "<Leader>he", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>", desc = "Harpoon (2)" },
-    { "<Leader>hi", "<cmd>lua require('harpoon.ui').nav_file(3)<CR>", desc = "Harpoon (3)" },
-    { "<Leader>ho", "<cmd>lua require('harpoon.ui').nav_file(4)<CR>", desc = "Harpoon (4)" },
-  },
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
+
+    harpoon:setup()
+
+    vim.keymap.set("n", "<Leader>ha", function()
+      harpoon:list():append()
+    end, { desc = "Harpoon (Append)" })
+
+    vim.keymap.set("n", "<Leader>hc", function()
+      harpoon:list():clear()
+    end, { desc = "Harpoon (Clear)" })
+
+    vim.keymap.set("n", "<Leader>hh", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = "Harpoon (Toggle)" })
+
+    vim.keymap.set("n", "<Leader>hn", function()
+      harpoon:list():select(1)
+    end, { desc = "Harpoon (1)" })
+
+    vim.keymap.set("n", "<Leader>he", function()
+      harpoon:list():select(2)
+    end, { desc = "Harpoon (2)" })
+
+    vim.keymap.set("n", "<Leader>hi", function()
+      harpoon:list():select(3)
+    end, { desc = "Harpoon (3)" })
+
+    vim.keymap.set("n", "<Leader>ho", function()
+      harpoon:list():select(4)
+    end, { desc = "Harpoon (4)" })
+  end,
 }
