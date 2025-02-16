c5fbd56a91af75fc615a35c8a10902fcde38f73c
Brandon Iffert <brandoniffert@gmail.com>
2025-02-16 04:10:52 -0500

    fix(nvim): update config to v0.12

--- nvim/lua/bti/plugins/blink.lua
+++ nvim/lua/bti/plugins/blink.lua
@@ -17,11 +17,6 @@ return {
       ["<CR>"] = { "accept", "fallback" },
       ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
       ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-
-      cmdline = {
-        ["<Tab>"] = { "show", "select_next", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
-      },
     },
 
     completion = {
@@ -50,9 +45,8 @@ return {
       },
     },
 
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-      cmdline = {},
+    cmdline = {
+      enabled = false,
     },
   },
 }
