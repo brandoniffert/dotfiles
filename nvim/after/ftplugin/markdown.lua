09e5bbb527d7dca09e7a51ffea76d0cd5b98963e
Brandon Iffert <brandoniffert@gmail.com>
2025-04-16 05:47:25 -0400

    feat(nvim): don't apply options for nofile buffers (hover documentation)

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,5 @@
-vim.opt_local.conceallevel = 0
-vim.opt_local.formatoptions:remove("t")
-vim.opt_local.spell = true
+if vim.bo.buftype ~= "nofile" then
+  vim.opt_local.conceallevel = 0
+  vim.opt_local.formatoptions:remove("t")
+  vim.opt_local.spell = true
+end
