badc01291b3acf9cd920e385c7050636bbaa3a7f
Brandon Iffert <brandoniffert@gmail.com>
2025-07-31 05:47:04 -0400

    feat(nvim): don't lazy load conform and nvim-lint

--- nvim/lua/bti/plugins/conform.lua
+++ nvim/lua/bti/plugins/conform.lua
@@ -1,7 +1,6 @@
 ---@type LazySpec
 return {
   "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
   opts = {
     formatters_by_ft = {
       astro = { "rustywind", "prettierd" },
--- nvim/lua/bti/plugins/nvim-lint.lua
+++ nvim/lua/bti/plugins/nvim-lint.lua
@@ -1,7 +1,6 @@
 ---@type LazySpec
 return {
   "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
   config = function()
     require("lint").linters_by_ft = {
       sh = { "shellcheck" },
