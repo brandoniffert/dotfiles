b29f681a09191f9b9fc59b7671aabb4ce2e4e228
Brandon Iffert <brandoniffert@gmail.com>
2025-12-13 04:06:29 -0500

    feat(zsh): show virtualenv information in prompt

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -37,6 +37,7 @@
     # =========================[ Line #2 ]=========================
     newline                 # \n
     background_jobs         # presence of background jobs
+    virtualenv
     user_prompt_char        # custom prompt char
   )
 
@@ -300,7 +301,7 @@
 
   ###[ virtualenv: python virtual environment (https://docs.python.org/3/library/venv.html) ]###
   # Python virtual environment color.
-  typeset -g POWERLEVEL9K_VIRTUALENV_FOREGROUND=37
+  typeset -g POWERLEVEL9K_VIRTUALENV_FOREGROUND=magenta
   # Don't show Python version next to the virtual environment name.
   typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=false
   # If set to "false", won't show virtualenv if pyenv is already shown.
