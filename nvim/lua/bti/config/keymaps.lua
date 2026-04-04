c81ab8c484f86586183f1363a0a59ca79748dcc6
Brandon Iffert <brandoniffert@gmail.com>
2026-04-04 07:09:56 -0400

    feat(nvim): add keymap for syncing plugins, update keymap for update plugins

--- nvim/lua/bti/config/keymaps.lua
+++ nvim/lua/bti/config/keymaps.lua
@@ -55,7 +55,13 @@ vim.keymap.set(
   require("bti.util.functions").replace_fancy_characters,
   { desc = "Replace fancy characters" }
 )
-vim.keymap.set("n", "<Leader>up", "<cmd>lua vim.pack.update()<CR>", { desc = "Update plugins" })
+vim.keymap.set("n", "<Leader>upp", "<cmd>lua vim.pack.update()<CR>", { desc = "Update plugins" })
+vim.keymap.set(
+  "n",
+  "<Leader>ups",
+  "<cmd>lua vim.pack.update(nil, { target = 'lockfile' })<CR>",
+  { desc = "Sync plugins (lockfile)" }
+)
 vim.keymap.set("n", "<Leader>ur", ":make!<CR>", { desc = "Run current file" })
 vim.keymap.set("n", "<Leader>uv", "V`]", { desc = "Select text that was just pasted" })
 vim.keymap.set("n", "<Leader>uw", require("bti.util.functions").strip_whitespace, { desc = "Strip whitespace" })
